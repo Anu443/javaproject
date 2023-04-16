6d4b1591540c5706fe24907258d3ab15b0fd1297
Anu443 <anuguru92@gmail.com>
2023-04-17 00:35:12 +0530

    Framework

--- index.js
+++
@@ -1,9 +0,0 @@
-console.log("Test");
-console.log("Check");
-console.log("Changed");
-console.log("Mixed");
-console.log("Good");
-console.log("Failed");
-console.log("bad");
-console.log("From develop");
-console.log("New Fix 123");
